7c8f19b032fed8417ed18012f0d2b4f85674d47b
axv50 <alexvega50@gmail.com>
2021-05-30 21:11:32 -0500

    html fixed

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,13 @@
 // Assignment code here
-
+var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*_-+=";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword=() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -15,3 +17,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+let characters = alpha;
+incNumbers.checked ? (characters += numbers) : "";
+incSymbols.checked ? (characters += symbols) : "";
+passwordTxt.value = generatePassword(length.value, characters);
+});
