9f2fb0f77a326460bd67d618d7b797c9b08eaf24
axv50 <alexvega50@gmail.com>
2021-05-30 22:07:11 -0500

    js added

--- Develop/script.js
+++ Develop/script.js
@@ -6,17 +6,33 @@ var symbols = "!@#$%^&*_-+=";
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var passwordTxt = document.querySelector("#password");
+var length = document.querySelector("#length");
+var incNumbers = document.querySelector("#numbers");
+var incSymbols = document.querySelector.("#symbols");
+
+
 // Write password to the #password input
-function writePassword=() {
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+  var generatePassword = (length, characters) => {
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return password;
+  };
+
+
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function writePassword);
 let characters = alpha;
 incNumbers.checked ? (characters += numbers) : "";
 incSymbols.checked ? (characters += symbols) : "";
